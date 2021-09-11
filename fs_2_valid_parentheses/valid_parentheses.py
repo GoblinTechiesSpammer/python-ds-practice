ee235b52ede39d87d78f6b5a89a413042e062259
Bailey <bailey.gage.davis@gmail.com>
2021-09-11 22:38:02 +0900

    fs_2 finished

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,14 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+    left_paren = 0
+    right_paren = 0
+    for paren in parens:
+        if paren == "(":
+            left_paren += 1
+        if paren == ")":
+            right_paren += 1
+            if left_paren == 0:
+                return False
+
+    return left_paren == right_paren
